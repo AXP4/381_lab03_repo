3c88a9b822c7df440b6e4e64f226556481c430f1
Kaitlyn <kaitlyn.dhamo@ucalgary.ca>
2026-02-13 12:24:18 -0700

    exercise3 done

---
+++ exercise3.js
@@ -0,0 +1,44 @@
+
+
+const classRoster = ["Alice", "Tom", "Charlie", "Diana", "Evan"];
+
+console.log(classRoster.toString());
+console.log(classRoster);
+
+classRoster.push("Fiona", "Nancy");
+const removedStudent = classRoster.shift();
+
+console.log(removedStudent);
+console.log(classRoster.length);
+
+const classInfo = {
+  className: "ENSF381: Full-Stack Web Development",
+  instructor: "Dr. Smith",
+  students: classRoster,
+  details: {
+    semester: "Winter",
+    year: 2025
+  }
+};
+
+classInfo.schedule = ["Monday", "Wednesday", "Friday"];
+classInfo.instructor = 'Dr.Abdellatif';
+
+console.log(classInfo.className);
+console.log(classInfo.instructor);
+console.log(classInfo.students);
+console.log(classInfo.details.semester);
+console.log(classInfo)
+
+const { className, students } = classInfo;
+console.log(className);
+console.log(students);
+
+const { details: { semester, year } } = classInfo;
+console.log(semester);
+console.log(year);
+
+const [student1, student2, ...remainingStudents] = classRoster;
+console.log(student1);
+console.log(student2);
+console.log(remainingStudents);
